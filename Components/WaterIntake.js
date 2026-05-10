fdc76e7a20613bfefae8c23cfa737251f5b7c9dd
himanshucode-07 <himanshusinghthakur23@gmail.com>
2026-05-11 00:42:13 +0530

    feat: add SVG hydration tracker with animated progress ring

--- Components/WaterIntake.js
+++ Components/WaterIntake.js
@@ -5,21 +5,34 @@ export class WaterIntake {
     this.span = document.querySelector(".count");
     this.resetBtn = document.querySelector(".resetBtn");
     this.count = 0;
+    this.progressCircle = document.querySelector(".water-progress");
+    this.maxGlasses = 8;
+    this.circumference = 282.7;
+  }
+  updateArc() {
+    const filled = (this.count / this.maxGlasses) * this.circumference;
+    const offset = this.circumference - filled;
+    this.progressCircle.style.strokeDashoffset = offset;
   }
   init() {
     this.plusBtn.addEventListener("click", () => {
-      this.count++;
-      this.span.innerText = this.count;
+      if (this.count < this.maxGlasses) {
+        this.count++;
+        this.span.innerText = this.count;
+        this.updateArc();
+      }
     });
     this.minusBtn.addEventListener("click", () => {
       if (this.count > 0) {
         this.count--;
         this.span.innerText = this.count;
+        this.updateArc();
       }
     });
     this.resetBtn.addEventListener("click", () => {
       this.count = 0;
       this.span.innerText = 0;
+      this.updateArc();
     });
   }
 }
